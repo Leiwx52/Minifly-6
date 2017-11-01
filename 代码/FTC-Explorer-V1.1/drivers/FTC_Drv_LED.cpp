8e825e83a626143077cbeeba1d70788e424f3250
WingsleyLui <915495167@qq.com>
2017-11-01 17:30:48 +0800

    6 axis

---
+++ 代码/FTC-Explorer-V1.1/drivers/FTC_Drv_LED.cpp
@@ -0,0 +1,50 @@
+/******************** (C) COPYRIGHT 2015 FTC ***************************
+ * 作者		 ：FTC
+ * 文件名  ：FTC_Drv_LED.cpp
+ * 描述    ：LED
+**********************************************************************************/
+#include "FTC_Drv_LED.h"
+
+FTC_LED led;
+
+void FTC_LED::Init(void)
+{
+	GPIO_InitTypeDef  GPIO_InitStructure1, GPIO_InitStructure2;
+
+	RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOC  , ENABLE  );
+	
+	GPIO_InitStructure1.GPIO_Pin  = GPIO_Pin_14;
+	GPIO_InitStructure1.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure1.GPIO_Speed = GPIO_Speed_50MHz;
+	
+	GPIO_InitStructure2.GPIO_Pin  = GPIO_Pin_13;
+	GPIO_InitStructure2.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure2.GPIO_Speed = GPIO_Speed_50MHz;
+	
+	GPIO_Init(GPIOC, &GPIO_InitStructure1);
+	GPIO_Init(GPIOC, &GPIO_InitStructure2);
+}
+
+void FTC_LED::ON1(void)
+{
+	GPIO_SetBits(GPIOC, GPIO_Pin_14);
+}
+
+void FTC_LED::ON2(void)
+{		
+	GPIO_SetBits(GPIOC, GPIO_Pin_13);
+}
+
+void FTC_LED::OFF1(void)
+{
+	GPIO_ResetBits(GPIOC, GPIO_Pin_14);
+}
+
+void FTC_LED::OFF2(void)
+{
+	GPIO_ResetBits(GPIOC, GPIO_Pin_13);
+}
+
+
+/******************* (C) COPYRIGHT 2015 FTC *****END OF FILE************/
+
